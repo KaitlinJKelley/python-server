aaf408791da5e779efb39167411c45c4da1cdee5
kjk1325e <kaitlinjkelley@gmail.comm>
2021-04-09 13:33:26 -0500

    successfully deleted smployee

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal, delete_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location
-from employees import get_all_employees, get_single_employee, create_employee
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
 import json
 
@@ -131,6 +131,8 @@ class HandleRequests(BaseHTTPRequestHandler):
             delete_animal(id)
         if resource == "locations":
             delete_location(id)
+        if resource == "employees":
+            delete_employee(id)
         if resource == "customers":
             delete_customer(id)
 
